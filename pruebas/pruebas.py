685b29329df0f82e3881a52dbd4176962a226bbc
Mauro Rodriguez <mr3453195@gmail.com>
2021-05-27 23:27:48 -0300

    pruebas

--- pruebas/pruebas.py
+++ pruebas/pruebas.py
@@ -1,12 +1,25 @@
 import curses
-
 stdscr=curses.initscr()
 
-curses.noecho()
+#para silenciar la tecla que precionamos
+#curses.noecho()
 
-key=stdscr.getch()
-stdscr.sddstr(str(key))
-key=stdscr.getch()
+#pondemos mostrar una tecla por comando
+#key=stdscr.getch()
+#stdscr.addstr(str(key))
+#key=stdscr.getch()
+
+#sirve para mover el cursor en x e y
+#stdscr.move(3, 10)
+#stdscr.getch()
+
+#para utilizar colores
+curses.start_color()
+#para darle el color: el primer parametro es el del texto y el segundo es el del fondo 
+curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
+stdscr.addnstr("hola mundo", curses.color_pair(3))
+stdscr.getch()
 
 
 curses.endwin()
